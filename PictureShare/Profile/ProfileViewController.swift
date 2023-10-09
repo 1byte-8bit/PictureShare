eb02e666ff7842ff5fa205a3ff1047030f1daf50
Aleksandr Prytkov <alexandr.prytkow@yandex.ru>
2023-10-09 09:08:16 +0300

    Code layout

--- PictureShare/Profile/ProfileViewController.swift
+++ PictureShare/Profile/ProfileViewController.swift
@@ -9,15 +9,134 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
     
-    @IBOutlet private weak var profileImage: UIImageView!
-    @IBOutlet private weak var nameLabel: UILabel!
+    private let profileImageView = UIImageView()
+    private let nameLabel = UILabel()
+    private let loginNameLabel = UILabel()
+    private let descriptionLabel = UILabel()
+    private let logoutButton = UIButton()
     
-    @IBOutlet private weak var loginNameLabel: UILabel!
-    @IBOutlet private weak var descriptionLabel: UILabel!
-    
-    @IBOutlet private weak var logoutButton: UIButton!
-    
-    @IBAction private func didTapLogoutButton(_ sender: UIButton) {
+    override func viewDidLoad() {         
+        super.viewDidLoad()
+        
+        view.backgroundColor = #colorLiteral(red: 0.1019607843, green: 0.1058823529, blue: 0.1333333333, alpha: 1)
+        
+        setupProfileImage()
+        view.addSubview(profileImageView)
+        
+        setupNameLabel()
+        view.addSubview(nameLabel)
+        
+        setupLoginNameLabel()
+        view.addSubview(loginNameLabel)
+        
+        setupDescriptionLabel()
+        view.addSubview(descriptionLabel)
+        
+        setupLogoutButton()
+        view.addSubview(logoutButton)
+        
+        setConstraints()
     }
     
+    private func setupProfileImage() {
+        let profileImage = UIImage(named: "Photo") ?? UIImage()
+        
+        profileImageView.image = profileImage
+        profileImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+    
+    private func setupNameLabel() {
+        nameLabel.text = "Екатерина Новикова"
+        nameLabel.font = nameLabel.font.withSize(23)
+        nameLabel.textColor = .white
+        
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func setupLoginNameLabel() {
+        loginNameLabel.text = "@ekaterina_nov"
+        loginNameLabel.font = loginNameLabel.font.withSize(13)
+        loginNameLabel.textColor = #colorLiteral(red: 0.6823529412, green: 0.6862745098, blue: 0.7058823529, alpha: 1)
+        
+        loginNameLabel.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func setupDescriptionLabel() {
+        descriptionLabel.text = "Hello, world!"
+        descriptionLabel.font = descriptionLabel.font.withSize(13)
+        descriptionLabel.textColor = .white
+        
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func setupLogoutButton() {
+        let buttonImage = UIImage(named: "logout") ?? UIImage()
+        
+        logoutButton.setImage(buttonImage, for: .normal)
+        logoutButton.imageEdgeInsets = UIEdgeInsets(
+            top: 11,
+            left: 16,
+            bottom: 11,
+            right: 6
+        )
+        logoutButton.addTarget(
+            self,
+            action: #selector(Self.didTapLogoutButton),
+            for: .touchUpInside
+        )
+        
+        logoutButton.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    @objc private func didTapLogoutButton() {
+        
+    }
+    
+}
+
+extension ProfileViewController {
+    private func setConstraints() {
+        NSLayoutConstraint.activate(
+            [
+                profileImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+                profileImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+                profileImageView.widthAnchor.constraint(equalToConstant: 70),
+                profileImageView.heightAnchor.constraint(equalToConstant: 70),
+            ]
+        )
+        
+        NSLayoutConstraint.activate(
+            [
+                nameLabel.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 8),
+                nameLabel.leadingAnchor.constraint(equalTo: profileImageView.leadingAnchor),
+                nameLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -8),
+            ]
+        )
+        
+        NSLayoutConstraint.activate(
+            [
+                loginNameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+                loginNameLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
+                loginNameLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -8),
+            ]
+        )
+        
+        NSLayoutConstraint.activate(
+            [
+                descriptionLabel.topAnchor.constraint(equalTo: loginNameLabel.bottomAnchor, constant: 8),
+                descriptionLabel.leadingAnchor.constraint(equalTo: loginNameLabel.leadingAnchor),
+                descriptionLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -8),
+            ]
+        )
+        
+        NSLayoutConstraint.activate(
+            [
+                logoutButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+                logoutButton.centerYAnchor.constraint(equalTo: profileImageView.centerYAnchor),
+                logoutButton.widthAnchor.constraint(equalToConstant: 44),
+                logoutButton.heightAnchor.constraint(equalToConstant: 44),
+            ]
+        )
+    }
 }
--- PictureShare/SingleImage/SingleImageViewController.swift
+++ PictureShare/SingleImage/SingleImageViewController.swift
@@ -9,19 +9,76 @@ import UIKit
 
 class SingleImageViewController: UIViewController {
     
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
     var image: UIImage! {
         didSet {
             guard isViewLoaded else { return }
             imageView.image = image
+            rescaleAndCenterImageInScrollView(image: image)
         }
     }
     
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupScrollView()
         imageView.image = image
+        rescaleAndCenterImageInScrollView(image: image)
+        
+    }
+    
+    @IBAction func didTapBackButton(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+    
+    @IBAction func didTapShareButton(_ sender: UIButton) {
+        
+        guard let image = image else { return }
+        
+        let activity = UIActivityViewController(
+            activityItems: [image],
+            applicationActivities: nil)
+        
+        present(activity, animated: true, completion: nil)
+    }
+    
+    func setupScrollView() {
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
     }
 
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        
+        view.layoutIfNeeded()
+        
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, max(hScale, vScale)))
+        
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        
+        let newContentSize = scrollView.contentSize
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        
+        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+    }
+
+}
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
 }
